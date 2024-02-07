14b0508c12946fb3b432b20f6d84ab35ffb2bc17
Sebastian <sebastian.rivas1991@hotmail.com>
2024-02-07 02:16:10 -0500

    End Section 16 - Guards, routes protection

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'auth-login-page',
   templateUrl: './login-page.component.html',
-  styles: [
-  ]
+  styleUrl: './login-page.component.css',
 })
 export class LoginPageComponent {
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ){}
+
+  onLogin(): void {
+    this.authService.login('sebastian@gmail.com','123456')
+      .subscribe( user => {
+        this.router.navigate(['/']);
+      });
+  }
 
 }
--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../auth/services/auth.service';
+import { User } from '../../../auth/interfaces/user.interface';
 
 @Component({
   selector: 'heroes-layout-page',
@@ -12,5 +15,19 @@ export class LayoutPageComponent {
     { label: 'Add', icon: 'add', url: './new-hero' },
     { label: 'Search', icon: 'search', url: './search' },
   ]
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  get user():User | undefined {
+    return this.authService.currentUser;
+  }
+
+  onLogout() {
+    this.authService.logout();
+    this.router.navigate(['/auth/login'])
+  }
 
 }
